6dac58a67d6d2494c3d1a03c2c20cfd8676cb157
sunnykurmi <sunnykurmiskks@gmail.com>
2024-02-29 22:23:37 +0530

    updated axios rout

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    // baseURL: "http://localhost:8080/",
+    baseURL: "https://internshala-backend-twn2.onrender.com/",
     withCredentials: true,
 });
 
