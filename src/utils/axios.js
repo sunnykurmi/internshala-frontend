7a218b5fb25c05a45650c664b91d5fbd98356b96
sunnykurmi <sunnykurmiskks@gmail.com>
2024-03-09 08:59:46 +0530

    change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "http://localhost:8080/",
-    baseURL: "https://internshala-backend-twn2.onrender.com/",
-    withCredentials: true,
+  baseURL: "https://internshala-backend-twn2.onrender.com/",
+  withCredentials: true,
 });
 
 export default instance;
